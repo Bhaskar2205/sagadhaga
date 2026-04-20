a59e0f3f75f6b7592ffab3fd73b94aab9d7c1ae1
Bhaskar Mamoria <pinkcityblog2000@gmail.com>
2026-04-20 12:02:32 +0530

    AI Agent deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { ReactNode } from "react";
 import SmoothScroll from "./components/SmoothScroll";
 import Navbar from "./components/Navbar";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -12,11 +13,21 @@ export default function RootLayout({
     <html lang="en">
       <body>
 
+        {/* NAVBAR */}
         <Navbar />
 
-        <SmoothScroll>
-          {children}
-        </SmoothScroll>
+        {/* PAGE CONTENT */}
+        <main className="pt-28 md:pt-32">
+          <SmoothScroll>
+            {children}
+          </SmoothScroll>
+        </main>
+
+        {/* TIDIO CHAT */}
+        <Script
+          src="//code.tidio.co/w8civs8ci2vwf7psvjotbvkj84uxtjiy.js"
+          strategy="afterInteractive"
+        />
 
       </body>
     </html>
